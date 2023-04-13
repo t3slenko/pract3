75c00fef68a0e52f9c0ef6b79292e31bee62a439
Maksim Teslenko <maksimteslenko57@gmail.com>
2023-04-13 18:14:57 +0300

    Test
    Test

---
+++ lab1/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IChairFactory chairFactory;
+            switch (comboBox1.SelectedItem)
+            {
+                case "Wooden":
+                    chairFactory = new WoodenChairFactory();
+                    break;
+                case "Metal":
+                    chairFactory = new MetalChairFactory();
+                    break;
+                case "Plastic":
+                    chairFactory = new PlasticChairFactory();
+                    break;
+                default:
+                    MessageBox.Show("Please select a material for the chair.");
+                    return;
+            }
+
+            IChair chair = chairFactory.CreateChair();
+            chair.Sit();
+        }
+    }
+}
---
+++ lab1/shoot.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public interface IChairFactory
+    {
+        IChair CreateChair();
+    }
+    public interface IChair
+    {
+        void Sit();
+    }
+    public class WoodenChairFactory : IChairFactory
+    {
+        public IChair CreateChair()
+        {
+            return new WoodenChair();
+        }
+    }
+
+    public class MetalChairFactory : IChairFactory
+    {
+        public IChair CreateChair()
+        {
+            return new MetalChair();
+        }
+    }
+
+    public class PlasticChairFactory : IChairFactory
+    {
+        public IChair CreateChair()
+        {
+            return new PlasticChair();
+        }
+    }
+
+    public class WoodenChair : IChair
+    {
+        public void Sit()
+        {
+            MessageBox.Show("You sit on a comfortable wooden chair.");
+        }
+    }
+
+    public class MetalChair : IChair
+    {
+        public void Sit()
+        {
+            MessageBox.Show("You sit on a cold, hard metal chair.");
+        }
+    }
+
+    public class PlasticChair : IChair
+    {
+        public void Sit()
+        {
+            MessageBox.Show("You sit on a flimsy plastic chair.");
+        }
+    }
+
+}
